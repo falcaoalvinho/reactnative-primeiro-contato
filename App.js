b7e36692efe1f43af3786d241d55d6b15854237a
Álvaro José <josealvaro3421@gmail.com>
2024-04-23 22:05:05 -0300

    Primeira vez usando REACT

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>(●'◡'●)</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: "black"
   },
+  title: {
+    color: "red",
+    fontSize: 100
+    
+  }
 });
